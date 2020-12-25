a43c5b1d78ce24a246bb293857a584d336712864
Chasmic Coder <chasmiccoder@gmail.com>
2020-12-25 16:57:52 +0530

    Updated alcazar
    Close to finishing tier 1 encrpytion of alcazar

--- Encryption-Algorithms/encrypt_alcazar.c
+++ Encryption-Algorithms/encrypt_alcazar.c
@@ -6,6 +6,51 @@ Its explanation can be found in the README file.
 
 #include <stdio.h>
 
-int main() {
+char* char_to_ascii_string( char character ) {
+    /*
+    This function takes a character and returns a string containing the ASCII form of that character.
+    Example: input = 'a', return value = '097'
+    */
+    int number = (int) character;
+    static char num_in_str_format[3] = "---";
+    int i = 2;
+
+    while ( number > 0 ) {
+        num_in_str_format[ i-- ] = number%10 + '0';
+        number = number / 10;
+    }
+
+    if (num_in_str_format[ 0 ] == '-') {
+        num_in_str_format[ 0 ] = '0';
+    }
     
+    return num_in_str_format;
+}
+
+
+char* tier_one_encryption( char* message, int len_message ) {
+    /*
+    Converting a string to its ascii form and separating each character's ascii string by white spaces.
+    Example: input = 'hi', return value = '104 105 '
+    */
+    char* new_message;
+    char delimiter = ' ';
+    int i = 0;
+    
+    for ( i = 0; i < len_message; i++ ) {
+        //char c = message + i;
+        ;
+
+    }
+    
+
+}
+
+int main() {
+    char* demo;
+    char c = 'A';
+    demo = char_to_ascii_string( c );
+    printf( "%s\n", demo );
+
+    return 0;   
 }
